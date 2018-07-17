b7b071580f27450644e9208c2ecfa494ec3961e1
niugrace <grace.niu.01@gmail.com>
2018-07-17 19:44:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,9 +16,16 @@ end
 get '/say/:number/:phrase' do 
   @number = params[:number]
   @phrase = params[:number]
-  "#{"
+end 
   
-get "s
+get '/say/:word1/:word2/:word3/:word4/:word5' do 
+  @word1 = params[:word1]
+  @word2 = params[:word2]
+  @word3= params[:word3]
+  @word4 = params[:word4]
+  @word5 = params[:word5]
+  "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
+end 
 
 
 
